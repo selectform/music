a1ae94aeccb04230a938674a59a2ea7d87a58cd3
selectform <2294733114@qq.com>
2019-02-27 19:47:12 +0800

    iconfont

--- src/pages/cc.vue
+++ src/pages/cc.vue
@@ -2,6 +2,7 @@
     <div class="cc">
         测试
         <mu-appbar style="width: 100%;" title="Title"></mu-appbar>
+        <span class="iconfont  icon-gengduo"></span>
     </div>
 </template>
 <script>
