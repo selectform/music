41c357646a40df7b07c5a053b92ce262a4de20c0
selectform <2294733114@qq.com>
2019-03-06 12:42:22 +0800

    轮播修改

--- src/pages/lineCentre/recommend/recommend.vue
+++ src/pages/lineCentre/recommend/recommend.vue
@@ -1,49 +1,44 @@
 <template><!----推荐-------->
-	<div>
+	<div class="slihead">
 		<div class="slider">
 			<div class="tui1">{{aa}}</div>
 			<mu-carousel hide-controls>
-				<mu-carousel-item>
-					<img :src="carouselImg1">
+				<mu-carousel-item v-for="item in items" :key="item.id">
+					<img :src="item.image">
 				</mu-carousel-item>
-				<!-- <mu-carousel-item>
-					<img :src="carouselImg2">
-				</mu-carousel-item>
-				<mu-carousel-item>
-					<img :src="carouselImg3">
-				</mu-carousel-item>
-				<mu-carousel-item>
-					<img :src="carouselImg4">
-				</mu-carousel-item> -->
-				</mu-carousel>
+			</mu-carousel>
+		</div>
+		<div class="slider">
+			<div class="tui1">{{bb}}</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import carouselImg1 from '../../../assets/images/sq.jpg';
-// import carouselImg2 from '../../../assets/images/sq.jpg';
-// import carouselImg3 from '../../../assets/images/sq.jpg';
-// import carouselImg4 from '../../../assets/images/sq.jpg';
 export default {
 	name: 'recommend',
 	data() {
 		return {
-			carouselImg1,
-			// carouselImg2,
-			// carouselImg3,
-			// carouselImg4,
-			aa: '精彩推荐'	
+			aa: '精彩推荐',	
+			bb: '推荐歌单',
+			items: [
+				{id: '1',image: require('../../../assets/images/sq.jpg')},
+				{id: '2',image: require('../../../assets/images/timg.jpg')},
+				{id: '3',image: require('../../../assets/images/timg1.jpg')},
+				{id: '4',image: require('../../../assets/images/timg2.jpg')},
+			],
 		}
-	}
+	},
 }
 </script>
 
 <style lang="scss">
+.slihead{
+	background: white;
+	min-height: 10rem;
 	.slider{
-		background: white;
 		width: 100%;
-		height: 3rem;
+		height: 2.6rem;
 		padding-top: 0.2rem;
 		.tui1{
 			height: 0.2rem;
@@ -52,5 +47,13 @@ export default {
 			line-height: 0.2rem;
 			color: #999;
 		}
+		.mu-carousel{
+		   top: 0.2rem;
+		   height:200px;
+		   .mu-carousel-item>img{
+			   height:100%;
+		   }
+		}
+	}
 	}
 </style>
